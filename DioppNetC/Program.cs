366c108d57fbbfb055870699b8e540856e29588c
FilipeMarcus <filipe.amorim@etec.sp.gov.br>
2021-04-26 12:42:10 -0300

    Adicionando exemplo 1 a pasta finalizada

---
+++ DioppNetC/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DioppNetC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numVezes = 5, meh = 33, mah = 22, mih = 11;
+
+            for (int i=0; i < numVezes; i++)
+            {   
+                // String interpolation funcionalidade do C Sharp
+                Console.Write($"Curso NET numero é: {i} {mah}, {meh}, {mih}");
+                Console.WriteLine($"\nQuebra de linha na nova Concatenacao: {i} {mih}, {mah}, {meh}\n");
+            }
+        }
+    }
+}   
